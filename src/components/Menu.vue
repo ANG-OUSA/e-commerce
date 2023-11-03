a1d53a35d2e68966c0f60d13433af52013bd317b
ANG-OUSA <angousa168@gmail.com>
2023-11-03 09:00:34 +0700

    TP2 starter

---
+++ src/components/Menu.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div>
+            <p class="feature">{{ menu_text }}</p>
+        </div>
+        <div>
+            <p></p>
+        </div>
+    </div>
+
+    
+</template>
+
+<script>
+export default {
+    name: "Menu",
+    props: {
+      menu_text: String,
+    },
+};
+
+</script>
+
+<style>
+.feature {
+    width: 432px;
+    height: 40px;
+    color: #253D4E;
+    font-size: 32px;
+
+
+}
+</style>
