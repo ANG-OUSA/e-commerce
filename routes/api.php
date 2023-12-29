52466156f7d743480868b8e4b4751c4c8aa55b56
ANG-OUSA <angousa168@gmail.com>
2023-12-29 16:13:52 +0700

    TP05

---
+++ routes/api.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/categories', function (Request $request) {
+    return "Get all categories";
+});
+Route::post('/categories', function (Request $request) {
+    return "Create 1 category";
+});
+Route::patch('/categories/{categoryId}', function (Request $request) {
+    return "Update 1 category";
+});
+Route::delete('/categories/{categoryId}', function (Request $request) {
+    return "Delete 1 category";
+});
+
+
+Route::get('/products', function (Request $request) {
+    return "Get all products";
+});
+Route::post('/products', function (Request $request) {
+    return "Create 1 product";
+});
+Route::patch('/products/{productId}', function (Request $request) {
+    return "Update 1 product";
+});
+Route::delete('/products/{productId}', function (Request $request) {
+    return "Delete 1 product";
+});
+
+Route::get('/categories/{categoryId}/products', function (Request $request) {
+    return "Delete 1 product";
+});
